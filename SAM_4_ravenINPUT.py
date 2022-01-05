28b6cceb6b5ef74a4311f0e77b0821eb82319c7f
VictMagal <victor.magalhaes@guiando.com.br>
2022-01-05 15:55:46 -0300

    commit 05/01 15:55

--- SAM_4_ravenINPUT.py
+++ SAM_4_ravenINPUT.py
@@ -1,33 +1,20 @@
 from pyravendb.store import document_store
 from IPython.display import display
 
-urls = "https://a.rdbguiando.ravendb.community"
+class Query():
+    def setUp (self):
+        urls = "https://a.rdbguiando.ravendb.community"
+        # use PFX file
+        cert = {"pfx": "C:/Users/Victor Magal/Downloads/Raven/victormagalhaes.client.certificate/victormagalhaes.client.certificate.pfx", "password": "#YpzIf&t3dby"}
+        self.store =  document_store.DocumentStore(urls=urls, database="automacao-faturas", certificate=cert)
+        self.store.initialize()
+          
+    def query_search (self):
+        with self.store.open_session() as session:
+            query_results = list(session.query().where(file_name="226542-8_20210901.pdf"))
+            display(query_results)
+            print(len(query_results))
 
-# #########################################
-# # use PFX file
-cert = {"pfx": "C:/Users/Victor Magal/Downloads/Raven/victormagalhaes.client.certificate/victormagalhaes.client.certificate.pfx", "password": "#YpzIf&t3dby"}
-# #########################################
-
-# # use PEM file
-# # cert = ("/path/to/cert.pem")
-
-# ###########################################
-# #use cert / key files
-# path = 'C:/Users/Victor Magal/Downloads/Raven/victormagalhaes.client.certificate'
-# cert = (f'{path}/victormagalhaes.client.certificate.crt', f'{path}/victormagalhaes.client.certificate.key')
-# ###########################################
-
-
-store =  document_store.DocumentStore(urls=urls, database="automacao-faturas", certificate=cert)
-store.initialize() 
-
-with store.open_session() as session:
-         query_results = list(session.query().where(file_name="226542-8_20210901.pdf"))
-         display(query_results)
-         print(len(query_results))
-
-# with store.open_session() as session:
-#     foo = session.load("foos/1")
-   
-#---------------------------------------------------------------------------------------------------------------------------------
-
+Query_start = Query()
+Query_start.setUp()
+Query_start.query_search()
